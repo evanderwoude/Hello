343322a4b499d3311da0bdc24bbe6850e6daf27e
evanderwoude <54680005+evanderwoude@users.noreply.github.com>
2019-08-29 17:13:29 -0500

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Eric Vander Woude and 
+ * Date: 2019/27/08
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Eric Vander Woude and _\n");
+
+  
+  return 0;
+}
